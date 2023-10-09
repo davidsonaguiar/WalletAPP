b9f5ac14185d370c9a208f0c2ba7b99d179f6f4d
davidson.05@outlook.com <davidson.05@outlook.com>
2023-10-08 22:13:17 -0300

    exit

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,10 +1,19 @@
 import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+
+  const navigate = useNavigate();
+
+  function exit() {
+    sessionStorage.removeItem("token");
+    navigate("/login");
+  }
+
   return(
     <header className="header-container">
       <h1 className="header-title">Wallet Web</h1>
-      <button className="header-button">
+      <button className="header-button" onClick={exit}>
         <BiLogOut />
         SAIR
       </button>
